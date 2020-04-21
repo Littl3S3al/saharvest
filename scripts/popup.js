a31d5f07dcb0410696d40e87762e14a6a5a45c8e
Littl3S3al <lauseal@gmail.com>
2020-04-21 12:18:36 +0200

    changes to pop up and added twitter

--- scripts/popup.js
+++ scripts/popup.js
@@ -24,7 +24,7 @@ popup.addEventListener('click', () => {
 });
 
 
-document.querySelector('.popupCopy').innerHTML = "<h3>Contact Alan on his private number: +27826511313<br><br>Or send him an email at <a href='mailto:alan@saharvest.org' class='popupLink'>alan@saharvest.org</a></h3>";
+document.querySelector('.popupCopy').innerHTML = "<h3>Contact OUR TEAM here: + 27 72 486 0618<br><br>Or send us an email at <a href='mailto:carab@saharvest.org' class='popupLink'>carab@saharvest.org</a></h3>";
 
 
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 AOS.init();
+
+document.querySelector('.mail').onclick = function () {
+    window.open('mailto:carab@saharvest.org');return false
+};
+document.querySelector('.phone').onclick = function () {
+    window.open('tel:+27724860618');return false
+};
+document.querySelector('.facebook').onclick = function () {
+    window.open('https://www.facebook.com/saharvest/');return false
+};
+document.querySelector('.twitter').onclick = function () {
+    window.open('https://twitter.com/saharvest1');return false
+};
